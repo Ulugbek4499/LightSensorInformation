83ae0693846cb95a46f02d4944525cb83fc2d658
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-12-12 11:10:21 +0500

    TelemetryControllerUnitTest

--- LightSensor.UnitTest/TelemetryControllerUnitTest.cs
+++ LightSensor.UnitTest/TelemetryControllerUnitTest.cs
@@ -1,142 +1,93 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
-using MediatR;
-using Microsoft.AspNetCore.Http;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Server.Controllers;
 using Server.DataBase;
 using Server.Entities;
 
-namespace LightSensor.UnitTest
+namespace LightSensor.UnitTest;
+
+public class TelemetryControllerUnitTest
 {
-    public class TelemetryControllerUnitTest
+    private readonly Mock<IApplicationDbContext> mockContext;
+    private readonly Mock<IMediator> mockMediator;
+
+    public TelemetryControllerUnitTest()
     {
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public async Task SaveTelemetryData_IfDeviceIsNullOrEmpty_ShouldReturnBadRequest(string deviceId)
+        mockContext = new Mock<IApplicationDbContext>();
+        mockMediator = new Mock<IMediator>();
+    }
+
+    [Fact]
+    public async Task SaveTelemetryData_Success()
+    {
+        // Arrange
+        mockContext.Setup(c => c.Telementries.Add(It.IsAny<Telemetry>())).Verifiable();
+        mockContext.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1); 
+
+        var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
+        var telemetryData = new List<TelemetryEntry>
         {
-            // Arrange
-            var mockContext = new Mock<IApplicationDbContext>();
-            var mockMediator = new Mock<IMediator>();
+         new TelemetryEntry
+          {
+              Time = 123456,
+              Illuminance = 10.0
+           }
+        };
+        var deviceId = "TestDevice";
 
-            var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
-            var telemetryData = new List<TelemetryEntry> 
-            { new TelemetryEntry { Time = 123456789, Illuminance = 10.0 } };
+        // Act
+        var result = await telemetryController.SaveTelemetryData(deviceId, telemetryData);
 
-            // Set up the HTTP context with a mocked User
-            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "TestUserId") };
-            var identity = new ClaimsIdentity(claims, "TestAuthType");
-            var claimsPrincipal = new ClaimsPrincipal(identity);
-            var httpContext = new DefaultHttpContext { User = claimsPrincipal };
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<OkObjectResult>(result);
 
-            telemetryController.ControllerContext = new ControllerContext
+        var okObjectResult = (OkObjectResult)result;
+
+        Assert.Equal(200, okObjectResult.StatusCode);
+        Assert.Equal("Telemetry data saved successfully", okObjectResult.Value);
+    }
+
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task SaveTelemetryData_Fail_WhenDeviceIdIsNullOrEmpty(string deviceId)
+    {
+        // Arrange
+
+        var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
+        var telemetryData = new List<TelemetryEntry> 
+        { 
+            new TelemetryEntry 
             {
-                HttpContext = httpContext
-            };
+                Time = 123456789, 
+                Illuminance = 10.0 
+            }
+        };
 
-            // Act
-            var result = await telemetryController.SaveTelemetryData(deviceId, telemetryData);
+        // Act
+        var result = await telemetryController.SaveTelemetryData(deviceId, telemetryData);
 
-            // Assert
-            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.Equal("Invalid device ID", badRequestResult.Value);
-        }
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal("Invalid device ID", badRequestResult.Value);
+    }
 
-        [Fact]
-        public async Task GetStatistics_WhenUserNotAuthenticated_ShouldReturnUnauthorized()
-        {
-            // Arrange
-            var mockContext = new Mock<IApplicationDbContext>();
-            var mockMediator = new Mock<IMediator>();
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task GetStatistics_Fail_WhenDeviceIdIsNullOrEmpty(string deviceId)
+    {
+        // Arrange
+        var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
 
-            var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
-            var deviceId = "1";
+        // Act
+        var result = await telemetryController.GetStatistics(deviceId);
 
-            // Set up the HTTP context with a non-authenticated user
-            var httpContext = new DefaultHttpContext { User = new ClaimsPrincipal() };
-
-            telemetryController.ControllerContext = new ControllerContext
-            {
-                HttpContext = httpContext
-            };
-
-            // Act
-            var result = await telemetryController.GetStatistics(deviceId);
-
-            // Assert
-            var objectResult = Assert.IsType<ObjectResult>(result);
-
-            // Assert that the status code is 401 (Unauthorized)
-            Assert.Equal(StatusCodes.Status401Unauthorized, objectResult.StatusCode);
-
-            // You can also assert other aspects of the result based on your specific requirements
-        }
-
-
-        [Fact]
-        public async Task SaveTelemetryData_WhenUserNotAuthenticated_ShouldNotReturnUnauthorized()
-        {
-            // Arrange
-            var mockContext = new Mock<IApplicationDbContext>();
-            var mockMediator = new Mock<IMediator>();
-
-            var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
-            var telemetryData = new List<TelemetryEntry> { new TelemetryEntry { Time = 123456789, Illuminance = 10.0 } };
-            var deviceId = "TestDevice";
-
-            // Set up the HTTP context with a non-authenticated user
-            var httpContext = new DefaultHttpContext { User = new ClaimsPrincipal() };
-
-            telemetryController.ControllerContext = new ControllerContext
-            {
-                HttpContext = httpContext
-            };
-
-            // Act
-            var result = await telemetryController.SaveTelemetryData(deviceId, telemetryData);
-
-            // Assert
-            Assert.IsNotType<UnauthorizedResult>(result);
-            // You can also assert other aspects of the result based on your specific requirements
-        }
-
-
-        [Fact]
-        public async Task SaveTelemetryData_IfInvalidTimeFormat_ShouldReturnInternalServerError()
-        {
-            // Arrange
-            var mockContext = new Mock<IApplicationDbContext>();
-            var mockMediator = new Mock<IMediator>();
-
-            var telemetryController = new TelemetryController(mockContext.Object, mockMediator.Object);
-            var telemetryData = new List<TelemetryEntry> { new TelemetryEntry { Time = -1, Illuminance = 10.0 } };
-            var deviceId = "TestDevice";
-
-            // Set up the HTTP context with a mocked User
-            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "TestUserId") };
-            var identity = new ClaimsIdentity(claims, "TestAuthType");
-            var claimsPrincipal = new ClaimsPrincipal(identity);
-            var httpContext = new DefaultHttpContext { User = claimsPrincipal };
-
-            telemetryController.ControllerContext = new ControllerContext
-            {
-                HttpContext = httpContext
-            };
-
-            // Act
-            var result = await telemetryController.SaveTelemetryData(deviceId, telemetryData);
-
-            // Assert
-            var statusCodeResult = Assert.IsType<ObjectResult>(result);
-            Assert.Equal(StatusCodes.Status500InternalServerError, statusCodeResult.StatusCode);
-            Assert.Equal("Internal Server Error", statusCodeResult.Value);
-        }
-
-     
-
-      
-
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal("Invalid device ID", badRequestResult.Value);
     }
 }
